4240b6393a37b42f456ba635c4fc5475c05a948a
chirag <chiragkaushik517@gmail.com>
2021-03-21 19:47:01 +0530

    Remote control with undo functionality: Light with on, off and undo last action functionality
      - Create Light which is to be turned on/off when button is pressed on remote.
      - Undo the last Command executed on the light.
      - Create LightOffCommand and LightOnCommand which stores on and off actions to be performed by Light.

---
+++ remote_control_with_undo/Light.java
@@ -0,0 +1,15 @@
+public class Light{
+    String place;
+
+    public Light(String place){
+        this.place = place;
+    }
+
+    public void on(){
+        System.out.println(this.place + " light is on.");
+    }
+
+    public void off(){
+        System.out.println(this.place + " light is off.");
+    }
+}
